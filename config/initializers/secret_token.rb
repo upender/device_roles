09cb3e83a87d8f5aefb5cb30773bf8f695e0725a
upender <upender@jasita.com>
2011-06-14 15:54:17 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceRoles::Application.config.secret_token = 'bc409682502e78823499193dc60b6c59fd47f3453b530a88a56578d5ee7d598b400d5e5e1caef49c5f9aa1e8c162162423e4325547504e1004df5a795f321570'
