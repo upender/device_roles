ef99694fc15acc3f43d3fdb75520228537e9c0cd
upender <upender@jasita.com>
2011-06-15 14:31:04 +0530

    commit cancan with device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DeviceRoles::Application.routes.draw do
+   devise_for :users, :path_names => { :sign_up => "register" }  
+
+  resources :projects
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ DeviceRoles::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
